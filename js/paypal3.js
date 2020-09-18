8e1327f9e8a7daf5a92bd0c8fd049dc2c32f5635
lethabo24 <lethabo24@gmail.com>
2020-09-18 18:13:18 +0200

    some weird stuff

---
+++ js/paypal3.js
@@ -0,0 +1,9 @@
+// Execute the payment
+onAuthorize: function(data, actions)
+{
+  return actions.payment.execute().then(function()
+  {
+    // Show a confirmation message to the buyer
+    window.alert('Thank you for your purchase!');
+  });
+}
